68c146d44dec6132b26011fcf60c9b5e06a39bbd
Kristoffer Risa <kristofferrisa@gmail.com>
2015-08-20 11:42:50 +0200

    Lagt til kommentater til javascript - navbar sticky

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
-$(document).ready(function () {
-
-    $(window).scroll(function () {
+$(document).ready(function () { 
+    //Kjøres når alt HTML innhold er ferdig lastet ned
+    $(window).scroll(function () {  
+        //Nav-bar sticky script ved scrolling
+        //Legger til classen "navbar-fixed" på elementet med id=nav når man har scrollet 143 pixels ned slik at navbar legges fast på toppen av siden           
         if ($(window).scrollTop() > 143) {
             $('#nav').addClass('navbar-fixed');
-        }
+        } 
         if ($(window).scrollTop() < 144) {
-            $('#nav').removeClass('navbar-fixed');
+            $('#nav').removeClass('navbar-fixed');        
         }
     });
 });
